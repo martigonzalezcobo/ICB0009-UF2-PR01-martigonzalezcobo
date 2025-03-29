5d5fc3e12db2a2d075eeca23b8e10adb655823f7
martigonzalezcobo <marti112001@gmail.com>
2025-03-29 20:44:24 +0100

    Ejercicio 1 Tarea 1 + Comentarios

---
+++ Ejercicio1/Tarea1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GestionAtencionHospitalaria
+{
+    class Program
+    {
+        static SemaphoreSlim semaforoMedicos = new SemaphoreSlim(4); // 4 médicos disponibles
+        static Random rnd = new Random();
+
+        static async Task AtenderPaciente(int numeroPaciente)
+        {
+            Console.WriteLine($"Paciente {numeroPaciente} ha llegado al hospital.");
+            await Task.Delay(numeroPaciente * 2000); // Simula el retraso de llegada (2s por paciente)
+
+            await semaforoMedicos.WaitAsync(); // Espera a que haya un médico disponible
+            int medicoAsignado = rnd.Next(1, 5); // Asigna un médico aleatorio del 1 al 4
+            Console.WriteLine($"Paciente {numeroPaciente} es atendido por el médico {medicoAsignado}.");
+
+            await Task.Delay(10000); // Simula consulta médica de 10s
+
+            Console.WriteLine($"Paciente {numeroPaciente} ha salido de consulta.");
+            semaforoMedicos.Release(); // Libera al médico
+        }
+
+        static async Task Main(string[] args)
+        {
+            List<Task> tareas = new List<Task>();
+            for (int i = 1; i <= 4; i++)
+            {
+                tareas.Add(AtenderPaciente(i));
+            }
+
+            await Task.WhenAll(tareas);
+            Console.WriteLine("Todas las consultas han finalizado.");
+        }
+    }
+}
